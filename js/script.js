12ccd9c1417c947dbf9398ea3f7e81cc02c9e9e7
Adwin Ying <adwinying@gmail.com>
2016-08-11 23:25:41 +0900

    Update site to Version 2
    Redesigned from the ground up
    - Single page 
    - Bootstrap
    - Retina support
    - Clean design
    - Lightweight

---
+++ js/script.js
@@ -0,0 +1,64 @@
+var main = function(){
+	$('button').click(function(){
+		$('.mobile-nav').toggleClass('show');
+	});
+
+	overlayOn = function(){
+		$(this).addClass("overlay");
+	};
+
+	overlayOff = function(){
+		$(this).removeClass("overlay");
+	};
+
+	$('#projects.section .image').hover(overlayOn, overlayOff);
+
+	$(window).scroll(function(){
+		var scroll 			= $(window).scrollTop();
+		var aboutTrigger	= $('#about.section').offset().top - 250;
+		var skillsTrigger	= $('#skills.section').offset().top - 250;
+		var projectsTrigger	= $('#projects.section').offset().top - 250;
+		var connectTrigger	= $('#connect.section').offset().top - 250;
+
+		if(scroll >= aboutTrigger){
+			$('#about.section').addClass("toggle");
+		}else{
+			$('#about.section').removeClass("toggle");
+		}
+
+		if(scroll >= skillsTrigger){
+			$('#skills.section').addClass("toggle");
+		}else{
+			$('#skills.section').removeClass("toggle");
+		}
+
+		if(scroll >= projectsTrigger){
+			$('#projects.section').addClass("toggle");
+		}else{
+			$('#projects.section').removeClass("toggle");
+		}
+
+		if(scroll >= connectTrigger){
+			$('#connect.section').addClass("toggle");
+		}else{
+			$('#connect.section').removeClass("toggle");
+		}
+	});
+
+	//Smooth Scrolling
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 500, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	
+}
+
+$(document).ready(main);
