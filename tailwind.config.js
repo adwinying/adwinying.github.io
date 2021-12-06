e2ba9ae34a3ee09bdd893dba2e65feec72899344
Adwin Ying <adwinying@gmail.com>
2021-12-06 17:10:28 +0900

    Restore tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+        title: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: () => ({
+        hero: "url('/img/hero_bg.jpg')",
+        "hero-mobile": "url('/img/hero_bg_mobile.jpg')",
+      }),
+      screens: {
+        hero: "800px",
+      },
+    },
   },
   variants: {
     extend: {},
