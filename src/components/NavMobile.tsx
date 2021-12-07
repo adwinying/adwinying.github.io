3fb4019aeac17e1d4adb4dc552ef71936e38322a
Adwin Ying <adwinying@gmail.com>
2021-12-07 20:02:09 +0900

    Rebuild Mobile Navigation component

---
+++ src/components/NavMobile.tsx
@@ -0,0 +1,17 @@
+import navLinks from "@/data/navLinks";
+
+export default function NavMobile() {
+  return (
+    <nav
+      className="flex flex-col sm:hidden divide-y divide-black
+        font-title font-medium tracking-wider uppercase
+        transition-all ease-in-out transition-duration-500"
+    >
+      {navLinks.map(({ name, url }) => (
+        <a className="w-full px-4 py-1.5 text-center" href={url}>
+          {name}
+        </a>
+      ))}
+    </nav>
+  );
+}
