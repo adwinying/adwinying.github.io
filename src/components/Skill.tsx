222c307e3cebe00d7270231e1752ffe1a45ad98e
Adwin Ying <adwinying@gmail.com>
2021-12-07 18:36:00 +0900

    Rebuild Skills component

---
+++ src/components/Skill.tsx
@@ -0,0 +1,40 @@
+import { Skill as SkillType } from "@/data/skills";
+import CodeIcon from "@/components/icons/Code";
+import ServerIcon from "@/components/icons/Server";
+import UserIcon from "@/components/icons/User";
+
+const skillIcons: Record<string, typeof UserIcon> = {
+  user: UserIcon,
+  server: ServerIcon,
+  code: CodeIcon,
+};
+
+type Props = SkillType & {};
+export default function Skill({ title, icon, contents }: Props) {
+  const SkillIcon = skillIcons[icon];
+
+  return (
+    <div className="flex-1 relative px-6 py-6 rounded-md border-2 border-black">
+      <div
+        className="flex justify-center items-center absolute top-0 left-1/2
+          w-10 h-10 bg-white rounded-full border-2 border-black transform
+          -translate-x-1/2 -translate-y-1/2"
+      >
+        <SkillIcon className="h-5" />
+      </div>
+
+      <h3
+        className="mt-1 mb-4 font-title text-xl text-center tracking-wider
+          uppercase"
+      >
+        {title}
+      </h3>
+
+      <ul className="space-y-3 text-xl font-light">
+        {contents.map((content) => (
+          <li>{content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/data/skills.ts
@@ -0,0 +1,52 @@
+export type Skill = {
+  title: string;
+  icon: string;
+  contents: string[];
+};
+
+const skills: Skill[] = [
+  {
+    title: "Client Side",
+    icon: "user",
+    contents: [
+      "HTML | HTML5",
+      "CSS | CSS3",
+      "TailwindCSS",
+      "JavaScript | ES6+",
+      "TypeScript",
+      "jQuery",
+      "React | Redux",
+      "Vue.js | Vuex",
+      "Inertia.js",
+    ],
+  },
+  {
+    title: "Server Side",
+    icon: "server",
+    contents: [
+      "PHP | Laravel",
+      "Node.js | Express.js",
+      "MySQL",
+      "PostgreSQL",
+      "MongoDB",
+      "Redis",
+      "Web Sockets (Socket.io)",
+    ],
+  },
+  {
+    title: "Others",
+    icon: "code",
+    contents: [
+      "C",
+      "CI/CD (Github Actions | GitlabCI)",
+      "Docker + Docker Compose",
+      "Vagrant",
+      "Ansible",
+      "Elastic Stack",
+      "LaTeX",
+      "Git (Github | Gitlab)",
+    ],
+  },
+];
+
+export default skills;
