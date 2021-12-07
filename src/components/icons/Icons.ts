0180a059d6115d1830d2212a1d91a6078bc80262
Adwin Ying <adwinying@gmail.com>
2021-12-07 19:40:14 +0900

    Rebuild Connect component

--- src/components/icons/Icons.ts
+++ src/components/icons/Icons.ts
@@ -1,6 +1,8 @@
 import CodeIcon from "@/components/icons/Code";
 import GithubIcon from "@/components/icons/Github";
 import LinkIcon from "@/components/icons/Link";
+import LinkedinIcon from "@/components/icons/Linkedin";
+import MailIcon from "@/components/icons/Mail";
 import MenuIcon from "@/components/icons/Menu";
 import ServerIcon from "@/components/icons/Server";
 import UserIcon from "@/components/icons/User";
@@ -9,6 +11,8 @@ const Icons: Record<string, typeof CodeIcon> = {
   code: CodeIcon,
   github: GithubIcon,
   link: LinkIcon,
+  linkedin: LinkedinIcon,
+  mail: MailIcon,
   menu: MenuIcon,
   server: ServerIcon,
   user: UserIcon,
