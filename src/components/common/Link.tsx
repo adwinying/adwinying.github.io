dadcba58b410de0c78f5fa0e214e892123a63c49
Adwin Ying <adwinying@gmail.com>
2022-04-22 11:44:49 +0900

    Extend Link components to support custom css classes

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -3,12 +3,13 @@ import { ReactNode } from "react";
 type Props = {
   href: string;
   ariaLabel?: string;
+  className?: string;
   children?: ReactNode;
 };
-export default function Link({ href, ariaLabel, children }: Props) {
+export default function Link({ href, ariaLabel, className, children }: Props) {
   return (
     <a
-      className="text-blue-400 hover:text-blue-700"
+      className={`text-blue-400 hover:text-blue-700 ${className}`}
       href={href}
       aria-label={ariaLabel}
     >
