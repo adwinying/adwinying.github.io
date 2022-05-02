4ab2b3b12761bdae6f91c6a271bd8f98699b7262
Adwin Ying <adwinying@gmail.com>
2022-05-02 18:11:58 +0900

    Implement RSS for blog posts

--- src/components/BlogHeader.tsx
+++ src/components/BlogHeader.tsx
@@ -1,10 +1,15 @@
 import Header from "@/components/Header";
+import RssIcon from "@/components/icons/Rss";
 
 export default function BlogHeader() {
   return (
     <Header>
       <a href="/blog" className="font-sans text-3xl font-normal text-gray-900">
         Blog
+      </a>
+
+      <a href="/blog/rss.xml" target="_parent">
+        <RssIcon className="mt-1 h-5" />
       </a>
     </Header>
   );
