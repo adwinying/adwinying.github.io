9624d4f18dc19dd075dd954015f603df18aafd2b
Adwin Ying <adwinying@gmail.com>
2022-05-14 22:21:02 +0900

    Dynamically generate footer year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="bg-black py-3 text-center text-gray-50">
-      iAdwin &copy; 2021 Adwin Ying.
+      iAdwin &copy; {year} Adwin Ying.
     </footer>
   );
 }
